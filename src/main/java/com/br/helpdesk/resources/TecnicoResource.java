a8c994f7775339aea7a553522d37b0158f902c56
Alc4nt4ra <felipealcantara1234@gmail.com>
2025-02-15 23:45:57 -0300

    delete para tecnico implementado

--- src/main/java/com/br/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/br/helpdesk/resources/TecnicoResource.java
@@ -10,6 +10,7 @@ import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,4 +63,15 @@ public class TecnicoResource {
 		Tecnico obj = service.update(id,objDTO);
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
+		 service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+
 }
+
+
+
--- src/main/java/com/br/helpdesk/services/TecnicoService.java
+++ src/main/java/com/br/helpdesk/services/TecnicoService.java
@@ -21,28 +21,27 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
 	}
-	
-	public List<Tecnico> findAll(){
+
+	public List<Tecnico> findAll() {
 		return repository.findAll();
 	}
-	
-	
+
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
 		validaCpfEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
-		  
+
 	}
-	
+
 	public Tecnico update(@Valid Integer id, TecnicoDTO objDTO) {
 		objDTO.setId(id);
 		Tecnico oldObj = findById(id);
@@ -51,20 +50,27 @@ public class TecnicoService {
 		return repository.save(oldObj);
 	}
 
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+		}
+		repository.deleteById(id);
+	}
+
 	private void validaCpfEmail(TecnicoDTO objDTO) {
-		
+
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
-		
-		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
 		}
-		
+
 		obj = pessoaRepository.findByEmail(objDTO.getEmail());
-		
-		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
 		}
 	}
 
-	
 }
